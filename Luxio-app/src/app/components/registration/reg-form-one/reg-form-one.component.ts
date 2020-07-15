1f5eb3ab8128c6dfe4725ebad90c4a0ce81f1b4c
julia30oren <juliaoren30@gmail.com>
2020-07-15 13:43:25 +0300

    +

--- Luxio-app/src/app/components/registration/reg-form-one/reg-form-one.component.ts
+++ Luxio-app/src/app/components/registration/reg-form-one/reg-form-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { RegistrationService } from 'src/app/services/registation/registration.service';
 
 @Component({
@@ -7,6 +8,19 @@ import { RegistrationService } from 'src/app/services/registation/registration.s
   styleUrls: ['./reg-form-one.component.css']
 })
 export class RegFormOneComponent implements OnInit {
+
+  public formTemplate = new FormGroup({
+    first_name: new FormControl('', Validators.required),
+    second_name: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phoneN: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    agreement: new FormControl(false, Validators.required)
+  })
+
+  public User_agreed: boolean;
+  public isSubmitted: boolean;
 
   constructor(
     private regService: RegistrationService
@@ -23,4 +37,21 @@ export class RegFormOneComponent implements OnInit {
     this.regService.AgreementPage();
   }
 
+  onSubmit(formValue) {
+    console.log('submit 1: ', formValue);
+    this.isSubmitted = true;
+
+    if (!formValue.agreement) {
+      console.log('false!!!!');
+      this.User_agreed = false;
+    } else {
+      console.log('go!');
+      this.User_agreed = true;
+    }
+
+  }
+
+  get formControls() {
+    return this.formTemplate['controls'];
+  }
 }
